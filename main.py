0ccbae1b51c529c6412682b7be52f238c6ff66fa
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2024-12-20 16:33:59 +0100

    Add Llama3.3 for prompt and backstory generation

--- main.py
+++ main.py
@@ -1,64 +1,82 @@
 import os
 import PIL
+import json
 import torch
 import requests
 from datetime import datetime
+from ollama import chat
+from diffusers import StableDiffusion3Pipeline
 from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
 
 
-def download_image(url):
-    image = PIL.Image.open(requests.get(url, stream=True).raw)
-    image = PIL.ImageOps.exif_transpose(image)
-    image = image.convert("RGB")
-    image.thumbnail((400, 400))
-    return image
+# TODO: Añadir más información de los usuarios. Crear un JSON/YAML.
+# Define person info
+person_info = {
+    "Name": "Carlos",
+    "Year of birth": "1993",
+    "Place of birth": "Málaga",
+}
 
+# Convert dictionary to a string (formatted for readability)
+person_info = json.dumps(person_info, indent=2)
 
-def load_image(path):
-    image = PIL.Image.open(path)
-    image = PIL.ImageOps.exif_transpose(image)
-    image = image.convert("RGB")
-    image.thumbnail((400, 400))
-    return image
-
-
-model_id = "timbrooks/instruct-pix2pix"
-pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
-pipe.to("cuda")
-pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
-
-url = "https://raw.githubusercontent.com/timothybrooks/instruct-pix2pix/main/imgs/example.jpg"
-img_file = "user_profiles/mrt.jpg"
-
-prompt = [
-    "Transform this person into an anime character",
-    "Transform this person into an anime character",
-    "Transform this person into an anime character",
-    "Transform this person into an anime character",
-    "Transform this person into a funny sketch",
-    "Transform this person into a caricature",
-    "Transform this person into a clown",
-    "Transform this person into a cowboy",
-    (
-        "Transform the person into a sinister, evil debtor, radiating an aura of greed and despair. Their face is pale and gaunt, with sunken cheeks, glowing red eyes, and a"
-        " twisted, menacing grin revealing sharp, jagged teeth. Their outfit is a tattered, dark suit covered in chains and glowing shackles, representing their eternal debt."
-        " Surrounding them is a swirling vortex of crumpled bills, overdue notices, and spectral coins that seem to whisper ominous threats. The background is a shadowy, decrepit"
-        " office filled with towering piles of decayed documents and broken calculators, illuminated only by flickering, greenish light emanating from a cursed ledger on their desk."
-        " The scene oozes tension and malevolence, capturing the torment of eternal financial doom."
-    ),
-]
-
-# image = download_image(url)
-image = load_image(img_file)
-
-generated_images = pipe(
-    prompt,
-    image=image,
-    num_inference_steps=30,
-    image_guidance_scale=1.25,
+# TODO: Checkear el GTI-GPT que hemos creado con una temperatura demasiado alta (20). Aún así los prompts siguen pareciéndose mucho, checkear qué está pasando.
+prompt = chat(
+    model="GTI-GPT",
+    messages=[
+        {
+            "role": "user",
+            "content": (
+                "Write a random prompt for a stable diffusion model to generate an image containing a demeaning representation of a person. The topic can be anything, be very"
+                " creative. Generate only the prompt, with no additional text or explanation, as I plan to simply copy and paste the response."
+            ),
+        },
+    ],
 )
 
-images = generated_images.images
+# TODO: Incorporar este script a la app del café.
+
+prompt = prompt["message"]["content"]
+print(prompt)
+
+backstory = chat(
+    model="llama3.3",
+    messages=[
+        {
+            "role": "user",
+            "content": (
+                f"Write an imaginary backstory, or lore, for a person that has become an evil debtor. You can use some of the information of that person: {person_info}. Also make"
+                f" use of additional information contained in a prompted I used to generate a funny image of this person: {prompt}. Be very specific with details, but overall do not"
+                " be too verbose, write a maximum of 2 paragraphs."
+            ),
+        },
+    ],
+)
+
+backstory = backstory["message"]["content"]
+print(backstory)
+
+
+# Stop ollama server
+os.system("ollama stop llama3.3")
+
+# Construct pipeline
+model_id = "stabilityai/stable-diffusion-3.5-large"
+pipe = StableDiffusion3Pipeline.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload()
+
+prompts = [prompt] * 3
+
+results = pipe(
+    prompts,
+    num_inference_steps=60,
+    guidance_scale=3.5,  # 3.5 - 5.5 -> The bigger this number the more the image will have to resemble the prompt
+    height=1024,
+    width=1024,
+    max_sequence_length=512,
+)
+
+images = results.images
 
 # Create a directory with the current timestamp
 timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
