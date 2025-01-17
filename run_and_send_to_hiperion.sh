bc52f736503903e25fc642967c929250c2a2e445
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2025-01-17 17:07:10 +0100

    Add TTS for backstories

--- run_and_send_to_hiperion.sh
+++ run_and_send_to_hiperion.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# Run the Python script
+# Run the first Python script
 /home/mrt/Projects/pix2pix/venv/bin/python /home/mrt/Projects/pix2pix/main.py
 
-# Check if the Python script succeeded
+# Check if the first Python script succeeded
 if [ $? -eq 0 ]; then
-    echo "Python script executed successfully. Proceeding with SCP."
+    echo "Backstories generated successfully. Proceeding with the TTS model and SCP to Hiperion."
 
-    # Copy the directory to the remote machine
-    scp -r /home/mrt/Projects/pix2pix/backstories hiperion:/home/mrt/Projects/pix2pix/
-    
+    # Run the second Python script
+    /home/mrt/Projects/coqui-ai-TTS/venv/bin/python /home/mrt/Projects/coqui-ai-TTS/GTI/coffee_backstories_tts.py
+
+    # Check if the second Python script succeeded
     if [ $? -eq 0 ]; then
-        echo "Directory copied successfully."
+        echo "TTS ran successfully. Proceeding with SCP to Hiperion."
+
+        # Copy the directory to the remote machine
+        scp -r /home/mrt/Projects/pix2pix/backstories hiperion:/home/mrt/Projects/pix2pix/
+
+        if [ $? -eq 0 ]; then
+            echo "Directory copied successfully."
+        else
+            echo "Error: Failed to copy the directory."
+        fi
     else
-        echo "Error: Failed to copy the directory."
+        echo "Error: Second Python script failed. Skipping SCP."
     fi
 else
-    echo "Error: Python script failed. Skipping SCP."
+    echo "Error: First Python script failed. Skipping subsequent steps."
 fi
-
